7ff8f25c57950680dc4b74c8cedc521e94a2ca5c
mladen-krstin <mladen.krstin@gmail.com>
2022-02-26 15:29:47 +0100

    d1

--- main-js.js
+++ main-js.js
@@ -1,1 +1,22 @@
-console.log('Zdravo');
+window.addEventListener('load', init);
+
+function init() {
+  let e = document.getElementById('ime');
+
+  e.addEventListener('keyup', proveriIme);
+}
+
+function proveriIme() {
+  let e = document.getElementById('ime');
+  let ime = e.ariaValueMax;
+
+  let poruka = '';
+
+  if (ime.length < 3) {
+    poruka = 'Ime nije ispravno!';
+  }
+
+  let pe = document.getElementById('poruka');
+
+  pe.innerHTML = poruka;
+}
