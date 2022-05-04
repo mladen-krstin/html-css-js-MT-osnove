60b519239d5df5c039ca1f3f4621b1e55623eaa8
mladen-krstin <mladen.krstin@gmail.com>
2022-05-04 23:01:48 +0200

    2
    new update

--- main-js.js
+++ main-js.js
@@ -1,22 +1,36 @@
-window.addEventListener('load', init);
+// window.addEventListener('load', init);
 
-function init() {
-  let e = document.getElementById('ime');
+// function init() {
+//   let e = document.getElementById('ime');
 
-  e.addEventListener('keyup', proveriIme);
-}
+//   e.addEventListener('keyup', proveriIme);
+// }
 
-function proveriIme() {
-  let e = document.getElementById('ime');
-  let ime = e.ariaValueMax;
+// function proveriIme() {
+//   let e = document.getElementById('ime');
+//   let ime = e.ariaValueMax;
 
-  let poruka = '';
+//   let poruka = '';
 
-  if (ime.length < 3) {
-    poruka = 'Ime nije ispravno!';
-  }
+//   if (ime.length < 3) {
+//     poruka = 'Ime nije ispravno!';
+//   }
 
-  let pe = document.getElementById('poruka');
+//   let pe = document.getElementById('poruka');
 
-  pe.innerHTML = poruka;
-}
+//   pe.innerHTML = poruka;
+// }
+
+var a = new String(“Hello”);
+
+var b = “Hello”;
+
+if (a === b) {
+
+  alert(“Same”);
+
+} else {
+
+  alert(“Different”);
+
+} 
